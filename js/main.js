0f6312d19672c2a75799c8bdc13b2cc531cb9dba
Arsenii Opalko <opalkoas@gmail.com>
2020-10-05 14:50:38 +0300

    delete comments

--- js/main.js
+++ js/main.js
@@ -13,35 +13,20 @@ $(function () {
   });
 });
 
-// Функция-обработчик, которую навешиваем на событие клика по ссылке
 function clickHandler(event) {
-  // Убираем стандартное поведение ссылки
   event.preventDefault();
-  // Берем элемент, по которому кликнули
   const elem = event.target;
-  // Берем значение атрибута 'category' у элемента
   const cat = elem.getAttribute('data-category');
-  // Находим все дочерние элементы
   const items = document.querySelectorAll('.activity__title-item');
-  // Для каждого дочернего элемента
   items.forEach((item) => {
-    // Если атрибут category совпадает с атрибутом элемента, по которому кликнули
     if (item.getAttribute('data-category') === cat) {
-      // Добавляем класс active
-      // Тут надо заметить что classList не добавляет новый элемент если такой же уже присутствует,
-      // т.е. не может быть 2х одинаковых классов. При работе например с массивами тут еще нужна
-      // проверка, что такой класс у него отсутствует.
       item.classList.add('activity__title-item--active');
     } else {
-      // Если не совпадает - удаляем класс active
       item.classList.remove('activity__title-item--active');
     }
   });
-  // Конец функции
 }
 
-// Находим все ссылки с нашим классом
 document.querySelectorAll('.activity__title-link').forEach((elem) => {
-  // Навешиваем на событие клика нашу функцию-обработчик
   elem.addEventListener('click', clickHandler);
 });
